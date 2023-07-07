6fb73df1a9a903ecca0b084f722e8cf5d816057b
deveshps <deveshps@dewsolutions.in>
2023-07-07 18:06:31 +0530

    c

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for plate 2
         </a>
       </header>
     </div>
